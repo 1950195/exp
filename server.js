8c94cb5c8c64ec1a4840b4202361eb84e5f3bae3
Lionel Dong <1950195@qq.com>
2013-08-01 13:30:20 +0800

    ip:127.0.0.1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 /*jslint anon:true, sloppy:true, nomen:true*/
 /*global process, require*/
 
-var ip      = process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0',
+var ip      = process.env.OPENSHIFT_INTERNAL_IP || '127.0.0.1',
     port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080,
     app     = require('mojito').createServer();
 app.listen(port, ip);
