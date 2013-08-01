7fe54dfbb30d662176f45e21c274ca498effc588
Lionel Dong <1950195@qq.com>
2013-08-01 14:49:10 +0800

    append context in server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@
 
 var ip      = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     port    = process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    app     = require('mojito').createServer();
-app.listen(port, ip);
+    app     = require('mojito').createServer({context: { environment: 'production' }});
+app.listen(ip, port);
