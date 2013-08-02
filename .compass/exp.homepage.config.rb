e608a05ca683cad8c5adc731d0a8d6ac9bc990cd
Lionel Dong <1950195@qq.com>
2013-08-02 09:18:58 +0800

    create compass config

---
+++ .compass/exp.homepage.config.rb
@@ -0,0 +1,8 @@
+project_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+http_path = "/"
+css_dir = "assets/css"
+http_stylesheets_dir = "/static/assets/css"
+sass_dir = "mojits/pages/homepage/assets/scss"
+additional_import_paths = ["assets/scss", "mojits/modules/header/assets/scss"]
+images_dir = "assets/images"
+http_generated_images_path = "/static/assets/images"
