23831c0671acbad40495504f91a547308db9f7c4
Lionel Dong <1950195@qq.com>
2013-08-05 14:50:45 +0800

    split headerBar from header, modify compass config

--- .compass/homepage.config.rb
+++ .compass/homepage.config.rb
@@ -3,6 +3,6 @@ http_path = "/"
 css_dir = "assets/css"
 http_stylesheets_dir = "/static/assets/css"
 sass_dir = "mojits/pages/homepage/assets/scss"
-additional_import_paths = ["assets/scss", "mojits/modules/header/assets/scss"]
+additional_import_paths = ["assets/scss", "mojits/modules"]
 images_dir = "assets/images"
 http_generated_images_path = "/static/assets/images"
---
+++ yui_modules/frameExec.server.js
@@ -0,0 +1,14 @@
+/*jslint anon:true, sloppy:true, nomen:true*/
+/*global YUI*/
+YUI.add('frame-exec', function(Y) {
+    Y.namespace('frameExec').done = function(ac, frameData, frameMeta) {
+        ac.composite.execute(ac.config.get(), function(data, meta) {
+            ac.assets.addAssets(meta.assets);
+            ac.deploy.constructMojitoClientRuntime(ac.assets, meta.binders);
+            ac.done(
+                Y.merge(data, ac.assets.renderLocations(), frameData),
+                Y.merge(meta, frameMeta)
+            );
+        });
+    };
+}, '0.0.1', {requires: []});
